0610dd7bbfc24dc3af7b39e169aab2ad8d99ae46
Merve Gürcan <mervegrcn1@gmail.com>
2023-02-28 12:46:31 +0300

    Pandas_Exercises
    Pandas library exercises for data science beginners

---
+++ casestudy_pandas_alistirmalar.py
@@ -0,0 +1,223 @@
+
+
+##################################################
+# Pandas Alıştırmalar
+##################################################
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+#########################################
+# Görev 1: Seaborn kütüphanesi içerisinden Titanic veri setini tanımlayınız.
+#########################################
+
+df = sns.load_dataset("titanic")
+
+#########################################
+# Görev 2: Yukarıda tanımlanan Titanic veri setindeki kadın ve erkek yolcuların sayısını bulunuz.
+#########################################
+
+df.head()
+df.groupby("sex")["sex"].count()
+
+#diger yol
+df["sex"].value_counts()
+
+#########################################
+# Görev 3: Her bir sutuna ait unique değerlerin sayısını bulunuz.
+#########################################
+
+df.iloc[:].nunique()
+
+#########################################
+# Görev 4: pclass değişkeninin unique değerleri bulunuz.
+#########################################
+
+df["pclass"].unique()
+
+
+#########################################
+# Görev 5:  pclass ve parch değişkenlerinin unique değerlerinin sayısını bulunuz.
+#########################################
+
+col_names = ["pclass", "parch"]
+df.loc[:, col_names].nunique()
+
+
+#########################################
+# Görev 6: embarked değişkeninin tipini kontrol ediniz. Tipini category olarak değiştiriniz. Tekrar tipini kontrol ediniz.
+#########################################
+
+df["embarked"].dtype
+
+df["embarked"] = df["embarked"].astype("category")
+df["embarked"].dtype
+
+#########################################
+# Görev 7: embarked değeri C olanların tüm bilgelerini gösteriniz.
+#########################################
+
+df.loc[df["embarked"] == "C", "survived":"alone"]
+
+#diger yol
+df.loc[df["embarked"] == "C"]
+
+df.head()
+#########################################
+# Görev 8: embarked değeri S olmayanların tüm bilgelerini gösteriniz.
+#########################################
+#1. yol
+df.loc[df["embarked"] != "S", "survived":"alone"]
+
+#kisa yol
+df.loc[df["embarked"] != "S"]
+
+#2. yol
+df.loc[~(df["embarked"] == "S"), "survived":"alone"]
+
+#kisa yol
+df.loc[~(df["embarked"] == "S")]
+
+#########################################
+# Görev 9: Yaşı 30 dan küçük ve kadın olan yolcuların tüm bilgilerini gösteriniz.
+#########################################
+
+df.loc[(df["age"] < 30) & (df["sex"] == "female"), "survived":"alone"]
+
+#kisa yol
+df.loc[(df["age"] < 30) & (df["sex"] == "female")]
+
+#########################################
+# Görev 10: Fare'i 500'den büyük veya yaşı 70 den büyük yolcuların bilgilerini gösteriniz.
+#########################################
+
+df.loc[(df["fare"] > 500) & (df["age"] > 70), "survived":"alone"]
+
+#kisa yol
+df.loc[(df["fare"] > 500) & (df["age"] > 70)]
+
+#########################################
+# Görev 11: Her bir değişkendeki boş değerlerin toplamını bulunuz.
+#########################################
+
+df.isnull().sum()
+
+#########################################
+# Görev 12: who değişkenini dataframe'den düşürün.
+#########################################
+
+df.drop("who", axis=1).head()
+df.head()
+
+#kalici silmek icin
+#df.drop("who", axis=1, inplace = True)
+
+#df=sns.load_dataset("titanic")
+
+#########################################
+# Görev 13: deck değikenindeki boş değerleri deck değişkenin en çok tekrar eden değeri (mode) ile doldurunuz.
+#########################################
+
+df["deck"]
+df["deck"].mode()
+
+df['deck'].fillna(df['deck'].mode()[0], inplace=True)
+df['deck'].isnull().values.any()
+
+#diger yol
+df = sns.load_dataset("titanic")
+
+df["deck"] = df["deck"].replace(to_replace=np.nan, value=df["deck"].mode()[0])
+df["deck"].isnull().values.any()
+
+#########################################
+# Görev 14: age değiskenindeki boş değerleri age değişkenin medyanı ile doldurun.
+#########################################
+
+df['age'].fillna(df['age'].median(), inplace=True)
+df['age'].isnull().values.any()
+
+#
+df = df.replace(to_replace = np.nan, value = df["age"].median())
+df.isnull().values.any()
+
+#########################################
+# Görev 15: survived değişkeninin Pclass ve Cinsiyet değişkenleri kırılımınında sum, count, mean değerlerini bulunuz.
+#########################################
+
+df.groupby(["pclass", "sex"]).agg({"survived": ["sum", "count", "mean"]})
+
+#########################################
+# Görev 16:  30 yaşın altında olanlar 1, 30'a eşit ve üstünde olanlara 0 vericek bir fonksiyon yazınız.
+# Yazdığınız fonksiyonu kullanarak titanik veri setinde age_flag adında bir değişken oluşturunuz oluşturunuz. (apply ve lambda yapılarını kullanınız)
+#########################################
+
+#fonksiyonla
+def thirty_flag(a):
+    if a<30:
+        return 1
+    else:
+        return 0
+
+df["age_flag"] = df["age"].apply(thirty_flag)
+
+#lambdayla
+df["age_flag"] = df["age"].apply(lambda x: 1 if x<30 else 0)
+
+#########################################
+# Görev 17: Seaborn kütüphanesi içerisinden Tips veri setini tanımlayınız.
+#########################################
+
+df2 = sns.load_dataset("tips")
+
+df2.head()
+df2.info()
+
+#########################################
+# Görev 18: Time değişkeninin kategorilerine (Dinner, Lunch) göre total_bill  değerlerinin toplamını, min, max ve ortalamasını bulunuz.
+#########################################
+
+df2.groupby(["time"]).agg({"total_bill":["min", "max", "mean"]})
+
+#########################################
+# Görev 19: Günlere ve time göre total_bill değerlerinin toplamını, min, max ve ortalamasını bulunuz.
+#########################################
+
+df2.groupby(["day", "time"]).agg({"total_bill":["min", "max", "mean"]})
+
+#########################################
+# Görev 20:Lunch zamanına ve kadın müşterilere ait total_bill ve tip  değerlerinin day'e göre toplamını, min, max ve ortalamasını bulunuz.
+#########################################
+
+df2.groupby(["day"]).agg({"total_bill":["min", "max", "mean"],
+                            "tip":["min", "max", "mean"]})
+
+df2.loc[(df2["sex"] == "Female") & (df2["time"] == "Lunch")].groupby(["day"]).agg({"total_bill": ["sum", "min","max", "mean"],
+                                                                                   "tip":["min", "max", "mean"]})
+
+#########################################
+# Görev 21: size'i 3'ten küçük, total_bill'i 10'dan büyük olan siparişlerin ortalaması nedir? (loc kullanin)
+#########################################
+
+df2.head()
+df2.loc[(df2["size"] < 3) & (df2["total_bill"] > 10)].mean()
+
+#########################################
+# Görev 22: total_bill_tip_sum adında yeni bir değişken oluşturun. Her bir müşterinin ödediği totalbill ve tip in toplamını versin.
+#########################################
+
+df2["total_bill_tip_sum"] = df2["total_bill"] + df2["tip"]
+df2.head()
+
+#########################################
+# Görev 23: total_bill_tip_sum değişkenine göre büyükten küçüğe sıralayınız ve ilk 30 kişiyi yeni bir dataframe'e atayınız.
+#########################################
+
+df2.sort_values("total_bill_tip_sum", ascending=0).head(30).reset_index()
+
+# veriler büyükten küçüğe sıralandıktan sonra, indekslerin 0 dan başlamasını sağlamak için reset_index kullandık
+# ascending = 1 olursa küçükten büyüğe sıralama olur
